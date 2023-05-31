f0169028e70df8d2f895e9fb8ddedf707ccee780
Ruben Sanchez Aparicio <rsanchezapa@uoc.edu>
2023-05-31 22:16:35 +0200

    update atomic class and extract all class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,36 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      white: "#ffffff",
+      black: "#0a0a0a",
+      textoAzul: "#000078",
+      textoAzulClaro: "#73edff",
+      figurasAjedrez: "#1616f3",
+    },
+    container: {},
+    fontFamily: {
+      font: ["-apple-system, BlinkMacSystemFont, Arial, sans-serif"],
+    },
+    screens: {
+      sm: "540px",
+      md: "720px",
+      lg: "960px",
+      xl: "1140px",
+      xxl: "1320px",
+    },
+    fontSize: {
+      base: "1rem",
+      lg: "1.2rem",
+      t2xl: "2rem",
+      t14xl: "1.4rem",
+    },
   },
+  extend: {
+    borderWidth: {
+      "3bpx": "3px",
+    },
+  },
+
   plugins: [],
 };
